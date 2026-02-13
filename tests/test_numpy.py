b1b853ff0f389458770866b8daf4ed89809d9598
TANG ZhiXiong <district10@users.noreply.github.com>
2026-02-13 18:23:34 +0800

    test ok (#2)
    * fix
    
    * fix
    
    * fix tests
    
    * fix
    
    ---------
    
    Co-authored-by: tang zhixiong <zhixiong.tang@momenta.ai>

--- tests/test_numpy.py
+++ tests/test_numpy.py
@@ -1,15 +1,5 @@
 import math
-import sys
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    import numpy as np
-else:
-    if sys.platform == 'win32':
-        path = 'E:/pocketpy/3rd/numpy/build/Release/numpy.dll'
-    else:
-        path = '/mnt/e/pocketpy/3rd/numpy/build/libnumpy.so'
-    np = __import__(path)
+import numpy as np
 
 def assert_equal(a, b):
     assert (a == b).all()
